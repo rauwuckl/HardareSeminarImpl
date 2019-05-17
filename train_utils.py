37b9c76149c28a1ca2cf6333cd19a38529958a07
Clemens Hutter <ch.23@web.de>
2019-05-17 12:54:04 +0200

    cuda

--- train_utils.py
+++ train_utils.py
@@ -41,7 +41,7 @@ def compute_accuracy(model, data_loader, device):
             output = model(img_device)
             prediction = torch.argmax(output, dim=-1)
 
-            correct = np.count_nonzero(prediction == label)
+            correct = np.count_nonzero(prediction == label.to(device))
 
             total_correct += correct
             total += len(label)
